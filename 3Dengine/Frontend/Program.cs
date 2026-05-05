3ba2a4b4aa2e4f13ca756add2285d47d456c546b
FrozenBlaze - Radon Team <engine00er@gmail.com>
2026-05-05 13:16:24 +0200

    Add files via upload

--- 3Dengine/Frontend/Program.cs
+++ 3Dengine/Frontend/Program.cs
@@ -1,67 +1,65 @@
 ﻿using System;
 using OpenTK.Graphics.OpenGL4;
-using SDL2;
+using OpenTK;
+using SDL3;
 using Render;
 
 class Initialize {
-
-
-        private static IntPtr GetProcAddress(string name) =>
-        SDL.SDL_GL_GetProcAddress(name);
-
-        private static void LoadGL()
+        class SDLBindingsContext : IBindingsContext
         {
-            var getProc = new Func<string, IntPtr>(GetProcAddress);
-            // OpenTK exposes GL.LoadBindings(Func<string, IntPtr>) in some builds:
-            var mi = typeof(GL).GetMethod("LoadBindings", new[] { typeof(Func<string, IntPtr>) });
-            if (mi != null) mi.Invoke(null, new object[] { getProc });
+            public IntPtr GetProcAddress(string procName)
+            {
+                return SDL.GLGetProcAddress(procName);
+            }
         }
 
-        static void Main(string[] args) {
+        static void Main() {
 
 
-            if(SDL.SDL_Init(SDL.SDL_INIT_VIDEO) < 0) {
-                Console.WriteLine(SDL.SDL_GetError());
+            if(SDL.Init(SDL.InitFlags.Video) == false) {
+                Console.WriteLine(SDL.GetError());
             }else{
                 Console.WriteLine("Initialized");
             }
+         
+            SDL.GLSetAttribute(SDL.GLAttr.ContextMajorVersion, 3);
+            SDL.GLSetAttribute(SDL.GLAttr.ContextMinorVersion, 3);
+            SDL.GLSetAttribute(SDL.GLAttr.ContextProfileMask, (int)SDL.GLProfile.Core);
 
-            SDL.SDL_GL_SetAttribute(SDL.SDL_GLattr.SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-            SDL.SDL_GL_SetAttribute(SDL.SDL_GLattr.SDL_GL_CONTEXT_MINOR_VERSION, 3);
-
-
-            IntPtr Window = SDL.SDL_CreateWindow("Engine",SDL.SDL_WINDOWPOS_CENTERED,SDL.SDL_WINDOWPOS_CENTERED, 100, 100, SDL.SDL_WindowFlags.SDL_WINDOW_OPENGL | SDL.SDL_WindowFlags.SDL_WINDOW_RESIZABLE);
-
-
-            IntPtr glContext = SDL.SDL_GL_CreateContext(Window);
-            LoadGL();
+            IntPtr Window = SDL.CreateWindow("Engine",
+            100, 100, SDL.WindowFlags.OpenGL | SDL.WindowFlags.Resizable);
+            SDL.SetWindowPosition(Window, Convert.ToInt32(SDL.WindowPosCentered()),Convert.ToInt32(SDL.WindowPosCentered()));
 
             if (Window == IntPtr.Zero)
             {
-                Console.WriteLine(SDL.SDL_GetError());
+                Console.WriteLine(SDL.GetError());
                 return;
             }
+            IntPtr glContext = SDL.GLCreateContext(Window);
+
+            if(SDL.GLMakeCurrent(Window, glContext) != true) {Console.WriteLine(SDL.GetError());}
+            GL.LoadBindings(new SDLBindingsContext());
             if (glContext == IntPtr.Zero) {
-                Console.WriteLine(SDL.SDL_GetError());
+                Console.WriteLine(SDL.GetError());
             }
             bool running = true;
-            SDL.SDL_Event e;
-            Objects.Context(100, 100);
+            SDL.Event e;
 
             while (running)
             {
-
-                while (SDL.SDL_PollEvent(out e) == 1)
+                while (SDL.PollEvent(out e) == true)
                 {
-                    if (e.type == SDL.SDL_EventType.SDL_QUIT)
+                    if (e.Type == (uint)SDL.EventType.Quit)
                         running = false;
                 }
-                SDL.SDL_GL_SwapWindow(Window);
+                SDL.GetWindowSize(Window, out int w, out int h);
+                Render.Objects.Context(w, h);
+                SDL.GLSwapWindow(Window);
             }
 
-            SDL.SDL_GL_DeleteContext(glContext);
-            SDL.SDL_DestroyWindow(Window);
-            SDL.SDL_Quit();
+            SDL.GLDestroyContext(glContext);
+            SDL.DestroyWindow(Window);
+            SDL.Quit();
 
 
         }
